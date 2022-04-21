8b35d8b447e16756b8f5e66c749ad6af74a4ad99
Leon06 <leondiego66@gmail.com>
2022-04-21 16:15:40 -0500

    Agregar archivos de proyecto.

---
+++ WSVenta/Models/Request/ClienteRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace WSVenta.Models.Request
+{
+    public class ClienteRequest
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
---
+++ WSVenta/Models/SistemaVentaContext.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WSVenta.Models
+{
+    public partial class SistemaVentaContext : DbContext
+    {
+        public SistemaVentaContext()
+        {
+        }
+
+        public SistemaVentaContext(DbContextOptions<SistemaVentaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cliente> Clientes { get; set; } = null!;
+        public virtual DbSet<Concepto> Conceptos { get; set; } = null!;
+        public virtual DbSet<Producto> Productos { get; set; } = null!;
+        public virtual DbSet<Ventum> Venta { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-224Q4TK;Database=SistemaVenta;Trusted_connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>(entity =>
+            {
+                entity.ToTable("cliente");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+            });
+
+            modelBuilder.Entity<Concepto>(entity =>
+            {
+                entity.ToTable("concepto");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Cantidad).HasColumnName("cantidad");
+
+                entity.Property(e => e.IdProducto).HasColumnName("id_producto");
+
+                entity.Property(e => e.IdVenta).HasColumnName("id_venta");
+
+                entity.Property(e => e.Importe)
+                    .HasColumnType("decimal(18, 0)")
+                    .HasColumnName("importe");
+
+                entity.Property(e => e.PrecioUnitario)
+                    .HasColumnType("decimal(18, 0)")
+                    .HasColumnName("precioUnitario");
+
+                entity.HasOne(d => d.IdProductoNavigation)
+                    .WithMany(p => p.Conceptos)
+                    .HasForeignKey(d => d.IdProducto)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_concepto_producto");
+
+                entity.HasOne(d => d.IdVentaNavigation)
+                    .WithMany(p => p.Conceptos)
+                    .HasForeignKey(d => d.IdVenta)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_concepto_venta");
+            });
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.ToTable("producto");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Costo)
+                    .HasColumnType("decimal(18, 0)")
+                    .HasColumnName("costo");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .HasColumnName("nombre");
+
+                entity.Property(e => e.PrecioUnitario)
+                    .HasColumnType("decimal(18, 0)")
+                    .HasColumnName("precioUnitario");
+            });
+
+            modelBuilder.Entity<Ventum>(entity =>
+            {
+                entity.ToTable("venta");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Fecha)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fecha");
+
+                entity.Property(e => e.IdCliente).HasColumnName("id_cliente");
+
+                entity.Property(e => e.Total)
+                    .HasColumnType("decimal(18, 0)")
+                    .HasColumnName("total");
+
+                entity.HasOne(d => d.IdClienteNavigation)
+                    .WithMany(p => p.Venta)
+                    .HasForeignKey(d => d.IdCliente)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_venta_cliente");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
